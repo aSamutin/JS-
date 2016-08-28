3ad766322af7471e7882543ffa5e39ec2a408da8
aSamutin <a.samutin@initlab.ru>
2016-08-28 20:43:55 +0300

    Убрал node_modules в backend из .gitignore

--- ProjectTasks/backend/client/bundle.js
+++ ProjectTasks/backend/client/bundle.js
@@ -10693,27 +10693,27 @@
 	module.exports = {
 	    getUsersList: function () {
 	        return fetch('/api/People')
-	        .then(response => response.json());
+	            .then(response => response.json());
 	    },
 	    getUser: function (login) {
 	        return fetch('/api/People?filter[where][login]='+login)
-	        .then(response => response.json());
+	            .then(response => response.json());
 	    },
 	    getTicket: function (ticketId) {
 	        return fetch('/api/Tickets?filter[where][id]='+ticketId)
-	        .then(response => response.json());
+	            .then(response => response.json());
 	    },
 	    getTicketsList: function (ticketsId) {
 	        let filter = {where: {id: {inq: ticketsId}}};
 	        filter = 'filter=' + encodeURIComponent(JSON.stringify(filter));
 	        return fetch(`/api/Tickets?${filter}`)
-	        .then(response => response.json());
+	            .then(response => response.json());
 	    },
 	    getComments: function (commentsId) {
 	        let filter = {where: {id: {inq: commentsId}}};
 	        filter = 'filter=' + encodeURIComponent(JSON.stringify(filter));
 	        return fetch(`/api/Comments?${filter}`)
-	        .then(response => response.json());
+	            .then(response => response.json());
 	    },
 	    editUser: function (newUser) {
 	        fetch('/api/People', {
